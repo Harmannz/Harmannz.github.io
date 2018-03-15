68fb312207556f16f41ff3e7319d5319004a2ff6
Harman Singh <harman.singh@hotmail.co.nz>
2018-03-15 23:26:00 +1300

    add temp github profile

---
+++ js/index.js
@@ -0,0 +1,8 @@
+(function($) {
+  "use strict";
+
+  // load the particles config
+  particlesJS.load('particles-js', 'assets/particlesjs-config.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
+})(jQuery);
